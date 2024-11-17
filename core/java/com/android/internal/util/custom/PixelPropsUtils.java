036f8b3b08f080371bc2fdac026dd90f7a9e61f7
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2024-11-17 23:49:41 +0700

    PixelPropsUtils: Update fingerprint to latest oriole beta

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -252,18 +252,18 @@ public class PixelPropsUtils {
     private static void spoofBuildGms() {
         // Alter build parameters to pixel for avoiding hardware attestation enforcement
         setPropValue("MANUFACTURER", "Google");
-        setPropValue("MODEL", "9 Pro XL");
-        setPropValue("FINGERPRINT", "google/komodo_beta/komodo:15/AP41.240823.009/12329489:user/release-keys");
+        setPropValue("MODEL", "Pixel 6");
+        setPropValue("FINGERPRINT", "google/oriole_beta/oriole:15/BP11.241025.006/12620009:user/release-keys");
         setPropValue("BRAND", "google");
-        setPropValue("PRODUCT", "komodo_beta");
-        setPropValue("DEVICE", "komodo");
+        setPropValue("PRODUCT", "oriole_beta");
+        setPropValue("DEVICE", "oriole");
         setPropValue("VERSION.RELEASE", "15");
-        setPropValue("ID", "AP41.240823.009");
-        setPropValue("VERSION.INCREMENTAL", "12329489");
+        setPropValue("ID", "BP11.241025.006");
+        setPropValue("VERSION.INCREMENTAL", "12620009");
         setPropValue("TYPE", "user");
         setPropValue("TAGS", "release-keys");
-        setPropValue("VERSION.SECURITY_PATCH", "2024-09-05");
-        setPropValue("VERSION.DEVICE_INITIAL_SDK_INT", "32");
+        setPropValue("VERSION.SECURITY_PATCH", "2024-11-05");
+        setPropValue("VERSION.DEVICE_INITIAL_SDK_INT", "21");
 }
     private static boolean isCallerSafetyNet() {
         return sIsGms && Arrays.stream(Thread.currentThread().getStackTrace())
